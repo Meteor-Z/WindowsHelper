86a9619537f3c2e0b01e9cc7c316fe03b1f5bde5
lzc <3030218083@qq.com>
2024-06-18 23:37:47 +0800

    更新QTreeWidget，做出初始模型

--- WindowsHelper/SpyWindow.cpp
+++ WindowsHelper/SpyWindow.cpp
@@ -8,7 +8,27 @@ SpyWindow::SpyWindow(QWidget *parent)
 {
 	ui.setupUi(this);
 
-	setStyleSheet("background-color: blue;");
+    m_MainLayout = new QHBoxLayout();
+    m_WindowTree = new QTreeWidget(this);
+   
+    m_WindowTree->setColumnCount(1);
+    QTreeWidgetItem* root = new QTreeWidgetItem(m_WindowTree, QStringList(QString("Root")));
+    QTreeWidgetItem* leaf = new QTreeWidgetItem(root, QStringList(QString("Leaf 1")));
+    root->addChild(leaf);
+    QTreeWidgetItem* leaf2 = new QTreeWidgetItem(root, QStringList(QString("Leaf 1")));
+    leaf2->setCheckState(0, Qt::Checked);
+    root->addChild(leaf2);
+    QList<QTreeWidgetItem*> rootList;
+    rootList << root;
+    m_WindowTree->insertTopLevelItems(0, rootList);
+
+
+    m_MainLayout->addWidget(m_WindowTree);
+    setLayout(m_MainLayout);
+    
+
+
+	
 }
 
 SpyWindow::~SpyWindow(){
--- WindowsHelper/SpyWindow.h
+++ WindowsHelper/SpyWindow.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <QWidget>
+#include <qtreewidget.h>
 #include "ui_SpyWindow.h"
+#include "QHBoxLayout"
 
 // 主窗口中的小窗口
 class SpyWindow : public QWidget
@@ -17,4 +19,7 @@ protected:
 
 private:
 	Ui::SpyWindowClass ui;
+	QTreeWidget* m_WindowTree{}; // 窗口的Tree
+	QHBoxLayout* m_MainLayout{}; // 左右布局
+
 };
